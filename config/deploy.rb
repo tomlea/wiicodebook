097402379df753f6d5d689ebe0d2c2a0857ed4f9
Tom Lea <contrib@tomlea.co.uk>
2009-01-10 22:22:56 +0000

    We are running with passenger, need to override capistrano default actions.

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,3 +19,15 @@ task :import_private_files do
 end    
 
 after "deploy:update_code", :import_private_files
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
