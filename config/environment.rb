7de06941a4912a008273ef349ceef7389c75ec47
Tom Lea <contrib@tomlea.co.uk>
2009-01-11 17:09:31 +0000

    No need to load anything for facebooker, it does the work for us.

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem "cwninja-facebooker", :lib => "facebooker", :source => "http://gems.github.com"
+  config.gem "cwninja-facebooker", :lib => false, :source => "http://gems.github.com"
   config.action_controller.session_store = :active_record_store
 end
