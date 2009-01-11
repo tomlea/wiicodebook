866ae64bd28fd3c03137e71814a173e161fb6436
Tom Lea <contrib@tomlea.co.uk>
2009-01-11 23:17:23 +0000

    Add factory girl for test env.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,3 +5,4 @@ config.action_controller.perform_caching             = false
 config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method = :test
 config.gem "sqlite3-ruby", :lib => "sqlite3"
+config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
