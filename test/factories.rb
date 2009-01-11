253eddee74cef7d767dfd7b9ffa8700233be047b
Tom Lea <contrib@tomlea.co.uk>
2009-01-11 23:48:41 +0000

    Factories should have been in the last commit.

--- test/factories.rb
+++ test/factories.rb
@@ -4,3 +4,12 @@ end
 Factory.define :wii_game do |g|
   g.name "Game With a Name"
 end
+
+Factory.sequence :wii_game_code do |i|
+  "%012i" % i
+end
+
+Factory.define :friend_code do |fc|
+  fc.game {|a| a.association(:wii_game) }
+  fc.friend_code { Factory.next :wii_game_code }
+end
